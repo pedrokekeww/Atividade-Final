69f59404180cedd5a5908694806831a3b9e26253
pedrokekeww <pedroveb119@gmail.com>
2024-09-27 20:38:59 -0300

    Update atv1.py

--- atv1.py
+++ atv1.py
@@ -1,26 +1,29 @@
-#Nome: Pedro Vinicius Ernesto Barbosa
-#Curso: P2 Informática
-#Prof.: Allan Kelvin
+# Nome: Pedro Vinicius Ernesto Barbosa
+# Curso: P2 InformC!tica
+# Prof.: Allan Kelvin
 temps = []
 abaixodamedia = []
 while True:
-  for i in range(10):
-    temperaturas = int(input(f"Entre com a {i+1}a temperatura: "))
-    if temperaturas < 15:
-      print("erro")
-      break
-    elif temperaturas > 40:
-      print("erro")
-      break
+    for i in range(10):
+        temperaturas = int(input(f"Entre com a {i+1}a temperatura: "))
+        if temperaturas < 15:
+            print("erro")
+            break
+        elif temperaturas > 40:
+            print("erro")
+            break
+        else:
+            temps.append(temperaturas)
+    if len(temps) < 10:
+        True
     else:
-      temps.append(temperaturas)
-  break
+        break
 tempmedia = sum(temps) / len(temps)
 for temperaturas in temps:
-  if temperaturas < tempmedia:
-    abaixodamedia.append(temperaturas)
-    
+    if temperaturas < tempmedia:
+        abaixodamedia.append(temperaturas)
+
 print(f"maior temperatura: {max(temps)}")
 print(f"menor temperatura: {min(temps)}")
-print(f"temperatura média: {tempmedia}")
-print(f"dias com temperaturas abaixo da média: {len(abaixodamedia)}")
+print(f"temperatura mC)dia: {tempmedia}")
+print(f"dias com temperaturas abaixo da mC)dia: {len(abaixodamedia)}")
